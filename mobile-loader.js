3ca0aa1dd54d4150d5e47770cf1367e3ec4a4031
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-09-07 02:06:33 -0300

    Preparar carregamento do layout mobile

---
+++ mobile-loader.js
@@ -0,0 +1,7 @@
+/* Carrega os ajustes mobile sem alterar a lógica do VM. */
+(function(){
+  var link=document.createElement('link');
+  link.rel='stylesheet';
+  link.href='mobile.css?v=20260907';
+  document.head.appendChild(link);
+})();
